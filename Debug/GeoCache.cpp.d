b6d76ca822e004b396c45e4da04bbd27ef824bc8
reponic tannous <reponic340@gmail.com>
2017-03-13 08:54:34 -0400

    Formulas Math

---
+++ Debug/GeoCache.cpp.d
@@ -0,0 +1,17 @@
+C:\Users\Reponic\AppData\Local\Temp\VMBuilds\GeoCache\uno\Debug\GeoCache.cpp.o: \
+ C:\Users\Reponic\AppData\Local\Temp\VMBuilds\GeoCache\uno\Debug\GeoCache.cpp \
+ E:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/arduino.h \
+ E:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ E:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ E:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ E:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ E:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ E:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ E:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ E:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ E:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ E:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\standard/pins_arduino.h \
+ E:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\ 14.0\Common7\IDE\Extensions\s03hukjq.rqr\Micro\ Platforms\default\debuggers\VM_DBG/VM_DBG.h \
+ E:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ E:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\SoftwareSerial\src/SoftwareSerial.h \
+ E:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h
